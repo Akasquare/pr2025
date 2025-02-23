8d3537ad8fec2df3bd048e8c859f617534115f7f
Aksquare <akashkush6272@gmail.com>
2025-02-23 13:49:25 +0530

    callbackhell

---
+++ callBackHell/app2.js
@@ -0,0 +1,29 @@
+
+
+function saveToDB(data,success,failure){
+    let internetSpeed = Math.floor(Math.random()*10) + 1;
+    if(internetSpeed > 4){
+        success();
+    }else {
+        failure();
+    }
+};
+
+saveToDB(
+    "hello",
+    ()=>{
+    console.log("success: your data was saved");
+    saveToDB(
+        "world",
+        ()=>{
+            console.log("success: your data was saved");
+        },
+        ()=>{
+            console.log("failure: week connection");
+        });
+
+    },
+    ()=>{
+        console.log("failure: week connection");
+    }
+);
